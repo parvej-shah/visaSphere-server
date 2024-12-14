e28adc244de543da457b66b738b11a28bbac8c96
parvej-shah <parvejshahlabib007@gmail.com>
2024-12-14 23:58:59 +0600

    Adding new user to the db using post.

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
+
 require('dotenv').config()
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 // Mongo DB Connections
-
-
+const uri = "mongodb+srv://parvejshahlabib007:bAsNONsEHNcaBbHY@newcluster.n9akf.mongodb.net/?retryWrites=true&w=majority&appName=newCluster";
 
 // Middleware Connections
 app.use(cors())
-app.use(express.json())
+app.use(express.json());
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    const userCollection = client.db("visaDB").collection("user");
+    const addedVisaCollection = client.db("visaDB").collection("addedVisa");
+    const appliedVisaCollection = client.db("visaDB").collection("appliedVisa");
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    app.post('/users',async(req,res)=>{
+        const user = req.body;
+        console.log('new user',user);
+        console.log("Headers:", req.headers); // Check headers
+        console.log("Body:", req.body); // Check request body
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+      })
+  } finally {
+    // Ensures that the client will close when you finish/error
+    /* await client.close(); */
+  }
+}
+run().catch(console.dir);
+
+
+
 
 
 // Routes
